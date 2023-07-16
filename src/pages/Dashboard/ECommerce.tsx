9c6afaa1fc08389b6fe493c82ce9feccb724c5aa
Wayfarerdesert <facundo355@outlook.com>
2023-07-16 14:41:39 +0200

    first commit

---
+++ src/pages/Dashboard/ECommerce.tsx
@@ -0,0 +1,40 @@
+import CardFour from '../../components/CardFour.tsx';
+import CardOne from '../../components/CardOne.tsx';
+import CardThree from '../../components/CardThree.tsx';
+import CardTwo from '../../components/CardTwo.tsx';
+import ChartOne from '../../components/ChartOne.tsx';
+import ChartThree from '../../components/ChartThree.tsx';
+import ChartTwo from '../../components/ChartTwo.tsx';
+import MapOne from '../../components/MapOne.jsx';
+import TableOne from '../../components/TableOne.tsx';
+
+import ChartFour from '../../components/ChartFour.tsx';
+
+const ECommerce = () => {
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
+        <CardOne />
+        <CardTwo />
+        <CardThree />
+        <CardFour />
+      </div>
+
+      <div className="mt-5 md:gap-6 2xl:gap-7.5">
+      <ChartFour />
+      </div>
+
+      <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
+        <ChartOne />
+        <ChartTwo />
+        <ChartThree />
+        <MapOne />
+        <div className="col-span-12">
+          <TableOne />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ECommerce;
